5a32c0c75104db6fa8cb8c40acbc69384227548b
ALATeacher <32556028+ALATeacher@users.noreply.github.com>
2017-11-07 14:57:28 -0800

    Added the player sprite to the center of the screen

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ import pygame
 import sys, os, math
 from pygame import *
 from random import randint
+from spriteHelper import SpriteSheet
 
 WINDOWWIDTH = 1024
 WINDOWHEIGHT = 768
@@ -13,34 +14,79 @@ GAMENAME = "Awesome Game"
 FRAMERATE = 60
 BGCOLOR = (255,255,255)
 
+class Player:
+    '''The class used to identify the single player'''
+    ##########VARIABLES##########
+    x = 0
+    y = 0
+    
+    collider = None
+    
+    ##########CONSTRUCTOR##########
+    def __init__(self):
+        self.image = SpriteSheet("p2_front.png")
+        self.image = self.image.get_image(0,0,66,92)
+    
+    def draw(self,surface):
+        '''draws the sprite for the player on the screen'''
+        surface.blit(self.image,(self.x,self.y))
+    
+    def getCollider(self):
+        '''returns a rectangle area representing the player sprite'''
+        pass
+    def getCenter(self):
+        rect = self.image.get_rect()
+        print(rect.x,rect.y)
+        return (rect.w/2,rect.h/2)
+
 class Game:
     ##########VARIABLES##########
     score = 0
     levels = []
-    
+    player = None
     
     ##########CONSTRUCTOR##########
     def __init__(self):
-        pass
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.surface = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
+        pygame.display.set_caption(GAMENAME)
+        self.player = Player()
     
     #########MAIN FUNCTION##########
     def main(self):
         playing = True
-        pygame.init()
-        clock = pygame.time.Clock()
-        self.surface = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
-        pygame.display.set_caption(GAMENAME)
+        
         
         ##########GAME LOOP##########
         while playing:
-            pass
+            delta = self.clock.tick(FRAMERATE)
+            for event in pygame.event.get():
+                if event.type==QUIT:
+                    self.quit()
+            self.processLogic(delta)
+            self.drawScreen()
+            pygame.display.flip()
+                    
+    def quit(self):
+        pygame.quit()
+        sys.exit()
         
+    def processLogic(self,delta):
+        pass
+    
+    def drawScreen(self):
+        self.surface.fill(BGCOLOR)
+        centerOfScreen = self.getCenterOfScreen()
+        centerOfPlayer = self.player.getCenter()
+        self.player.x = centerOfScreen[0]-centerOfPlayer[0]
+        self.player.y = centerOfScreen[1]-centerOfPlayer[1]
+        self.player.draw(self.surface)
+    
+    def getCenterOfScreen(self):
+        return (WINDOWWIDTH/2,WINDOWHEIGHT/2)
         
         
 if __name__=='__main__':
     game = Game()
     game.main()
-        
-    
-
-
